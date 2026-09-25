c258ce2fad63c392b23b180782141c483f17226c
Morenoreis <morenoreis@gmail.com>
2026-09-24 22:01:17 -0300

    Fix: run headless in CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     baseURL: 'https://opensource-demo.orangehrmlive.com',
-    headless: false,
+    headless: !!process.env.CI,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
